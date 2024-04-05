735686b152e24e2381a9177025b1228e58435426
Segni <segniassaye2@gmail.com>
2024-04-05 00:13:22 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,11 @@
 Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
+
 from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['54.89.109.87', '100.25.190.21']
+
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
